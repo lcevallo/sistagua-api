3d148272a03a01bcc63ceef48abf51c35a66b46b
Luis <lcevallo@gmail.com>
2021-02-04 16:19:31 -0500

    Ya me falta poco
    Seguimos con el post de ficha tecnica detalle

--- resources/sql/lc_spGuardarFichaTecnica.sql
+++ resources/sql/lc_spGuardarFichaTecnica.sql
@@ -3,7 +3,7 @@ DELIMITER $$
 CREATE PROCEDURE lc_spGuardarFichaTecnica(
     _id_FichaTecnica int,
     IN pDetallesJson JSON,
-    OUT pJson_ids JSON
+    OUT pJson_ids TEXT
 )
 BEGIN
     DECLARE vJsonEsValido INT;
@@ -27,38 +27,52 @@ BEGIN
         DECLARE vUpdated_at DATETIME;
         DECLARE vPublish TINYINT;
 
-        SET vJsonEsValido = JSON_VALID(pParametroJson);
+        SET vJsonEsValido = JSON_VALID(pDetallesJson);
+        SET pJson_ids = '[';
 
-        IF vJsonEsValido = 0 THEN 
-        # El objeto JSON no es válido, salimos prematuramente
-        SELECT "JSON suministrado no es válido";
+        IF EXISTS (SELECT 1 FROM `ficha_tecnica` where `ficha_tecnica`.`id` = _id_FichaTecnica  AND `ficha_tecnica`.`publish` = true) THEN
+
+            IF vJsonEsValido = 0 THEN 
+                # El objeto JSON no es válido, salimos prematuramente
+                SELECT "JSON suministrado no es válido";
+                SET pJson_ids = '0';
+            ELSE
+                    # Nuestro objeto es válido, podemos proceder
+                    SET vItems = JSON_LENGTH(pDetallesJson);
+                    # El objeto es válido y contiene al menos un elemento
+                    IF vItems > 0 THEN 
+        
+                        WHILE vIndex < vItems DO
+        
+                            SET vFactura=  JSON_UNQUOTE(JSON_EXTRACT(pDetallesJson, CONCAT('$[', vIndex, '].factura')));
+                            SET vFecha_mantenimiento=  JSON_UNQUOTE(JSON_EXTRACT(pDetallesJson, CONCAT('$[', vIndex, '].fecha_mantenimiento')));
+                            SET vRecibo=  JSON_UNQUOTE(JSON_EXTRACT(pDetallesJson, CONCAT('$[', vIndex, '].recibo')));
+                            SET vFicha_tecnica=  JSON_UNQUOTE(JSON_EXTRACT(pDetallesJson, CONCAT('$[', vIndex, '].ficha_tecnica')));
+                            SET vDescripcion=  JSON_UNQUOTE(JSON_EXTRACT(pDetallesJson, CONCAT('$[', vIndex, '].descripcion')));
+                            SET vPersona_recepta=  JSON_UNQUOTE(JSON_EXTRACT(pDetallesJson, CONCAT('$[', vIndex, '].persona_recepta')));
+                            SET vFirma_url=  JSON_UNQUOTE(JSON_EXTRACT(pDetallesJson, CONCAT('$[', vIndex, '].firma_url')));
+                            SET vCedula_receptor=  JSON_UNQUOTE(JSON_EXTRACT(pDetallesJson, CONCAT('$[', vIndex, '].cedula_receptor')));
+                            SET vPersona_dio_mantenimiento=  JSON_UNQUOTE(JSON_EXTRACT(pDetallesJson, CONCAT('$[', vIndex, '].persona_dio_mantenimiento')));
+                            SET vCedula_dio_mantenimiento=  JSON_UNQUOTE(JSON_EXTRACT(pDetallesJson, CONCAT('$[', vIndex, '].cedula_dio_mantenimiento')));
+        
+                            INSERT INTO ficha_tecnica_detalle (fk_ficha_tecnica, factura, fecha_mantenimiento, recibo, ficha_tecnica, descripcion, persona_recepta, firma_url, cedula_receptor, persona_dio_mantenimiento, cedula_dio_mantenimiento)
+                            VALUES (_id_FichaTecnica, vFactura, vFecha_mantenimiento, vRecibo, vFicha_tecnica, vDescripcion, vPersona_recepta, vFirma_url, vCedula_receptor, vPersona_dio_mantenimiento, vCedula_dio_mantenimiento);
+                            SET last_inserted_ficha_tecnica_detalle_id = LAST_INSERT_ID();
+        
+                            -- SET pJson_ids = JSON_ARRAY_APPEND(pJson_ids, '$', CONCAT('{"id":', last_inserted_ficha_tecnica_detalle_id,','));
+                            IF vIndex = 0 THEN    
+                                SET pJson_ids = CONCAT(pJson_ids,'',last_inserted_ficha_tecnica_detalle_id);
+                            ELSE
+                                SET pJson_ids = CONCAT(pJson_ids,',',last_inserted_ficha_tecnica_detalle_id);
+                            END IF;
+                            SET vIndex = vIndex + 1;
+                        END WHILE;
+                        SET pJson_ids = CONCAT(pJson_ids,']');
+                    END IF;
+            END IF;
         ELSE
-                # Nuestro objeto es válido, podemos proceder
-                SET vItems = JSON_LENGTH(pParametroJson);
-                # El objeto es válido y contiene al menos un elemento
-                IF vItems > 0 THEN 
-    
-                    WHILE vIndex < vItems DO
-    
-                        SET vFactura=  JSON_UNQUOTE(JSON_EXTRACT(pParametroJson, CONCAT('$[', vIndex, '].factura')));
-                        SET vFecha_mantenimiento=  JSON_UNQUOTE(JSON_EXTRACT(pParametroJson, CONCAT('$[', vIndex, '].fecha_mantenimiento')));
-                        SET vRecibo=  JSON_UNQUOTE(JSON_EXTRACT(pParametroJson, CONCAT('$[', vIndex, '].recibo')));
-                        SET vFicha_tecnica=  JSON_UNQUOTE(JSON_EXTRACT(pParametroJson, CONCAT('$[', vIndex, '].ficha_tecnica')));
-                        SET vDescripcion=  JSON_UNQUOTE(JSON_EXTRACT(pParametroJson, CONCAT('$[', vIndex, '].descripcion')));
-                        SET vPersona_recepta=  JSON_UNQUOTE(JSON_EXTRACT(pParametroJson, CONCAT('$[', vIndex, '].persona_recepta')));
-                        SET vFirma_url=  JSON_UNQUOTE(JSON_EXTRACT(pParametroJson, CONCAT('$[', vIndex, '].firma_url')));
-                        SET vCedula_receptor=  JSON_UNQUOTE(JSON_EXTRACT(pParametroJson, CONCAT('$[', vIndex, '].cedula_receptor')));
-                        SET vPersona_dio_mantenimiento=  JSON_UNQUOTE(JSON_EXTRACT(pParametroJson, CONCAT('$[', vIndex, '].persona_dio_mantenimiento')));
-                        SET vCedula_dio_mantenimiento=  JSON_UNQUOTE(JSON_EXTRACT(pParametroJson, CONCAT('$[', vIndex, '].cedula_dio_mantenimiento')));
-    
-                        INSERT INTO ficha_tecnica_detalle (fk_ficha_tecnica, factura, fecha_mantenimiento, recibo, ficha_tecnica, descripcion, persona_recepta, firma_url, cedula_receptor, persona_dio_mantenimiento, cedula_dio_mantenimiento)
-                        VALUES (_id_FichaTecnica, vFactura, vFecha_mantenimiento, vRecibo, vFicha_tecnica, vDescripcion, vPersona_recepta, vFirma_url, vCedula_receptor, vPersona_dio_mantenimiento, vCedula_dio_mantenimiento);
-                        SET last_inserted_ficha_tecnica_detalle_id = LAST_INSERT_ID();
-    
-                        SET pJson_ids = JSON_ARRAY_APPEND(pJson_ids, '$', CONCAT('{"id":', last_inserted_ficha_tecnica_detalle_id,','));
-                    END WHILE;
-                END IF;
-           END IF;
+            SET pJson_ids = '-1';
+        END IF;
 END
 $$
 DELIMITER;
---
+++ resources/sql/test_lc_spGuardarFichaTecnica.sql
@@ -0,0 +1,40 @@
+CALL sistagua_bd.lc_spGuardarFichaTecnica(1,'[ { 
+ "factura": "#R456JUR", 
+ "fecha_mantenimiento": "2016-02-04", 
+ "recibo": "Juan Garces", 
+ "ficha_tecnica": "FT-245784", 
+ "descripcion": "Se ha cambiado la valvula TE45 para poder usar una nueva con mayor capacidad", 
+ "persona_recepta": "Juan Garces", 
+ "firma_url": "http://ikki.png", 
+ "cedula_receptor": "0915748795", 
+ "persona_dio_mantenimiento": "Rafael Valdez", 
+ "cedula_dio_mantenimiento": "0915748789"
+ },
+ {
+ 
+ "factura": "#R456J787", 
+ "fecha_mantenimiento": "2016-03-04", 
+ "recibo": "Juan Piguave", 
+ "ficha_tecnica": "FT-784", 
+ "descripcion": "Se ha cambiado el oxigeno", 
+ "persona_recepta": "Juan Cevallos", 
+ "firma_url": "http://ikki2.png", 
+ "cedula_receptor": "0915748777", 
+ "persona_dio_mantenimiento": "Rafael Valdez", 
+ "cedula_dio_mantenimiento": "0915747895"
+ },
+ {
+ 
+ "factura": "#FERST56", 
+ "fecha_mantenimiento": "2017-05-04", 
+ "recibo": "Juan Garces II", 
+ "ficha_tecnica": "FT-7847458", 
+ "descripcion": "Ahora se daño esta huevada", 
+ "persona_recepta": "Gabriel Cevallos", 
+ "firma_url": "http://bulbaousr.png", 
+ "cedula_receptor": "0915748795", 
+ "persona_dio_mantenimiento": "Rafael Valdez", 
+ "cedula_dio_mantenimiento": "0915747877"
+ }]',
+@json_respuesta);
+SELECT @json_respuesta;
